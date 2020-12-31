c335cb9b378e90b71e3f57672e99e32647f87c8c
Juliano Costa Silva <apfjuliano@gmail.com>
2020-12-31 01:04:08 -0300

    3.Setting Up Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+//REDUX SETUP
+import { createStore } from "redux"
+import rootReducer from "./reducers"
+import { Provider } from "react-redux"
+
+const store = createStore(
+  rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -13,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
