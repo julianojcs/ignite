b926acffccc79b87be04a25a2ec9e19e663bb90c
Juliano Costa Silva <apfjuliano@gmail.com>
2021-01-14 21:39:53 -0300

    20.Adding The Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+//Components and Pages
 import Home from './pages/Home'
+import Nav from './components/Nav'
+//Styles
 import GlobalStyles from './components/GlobalStyles'
+//router
 import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyles />
+      <Nav />
       <Route path={['games/:id', '/']} component={Home} />
     </div>
   )
